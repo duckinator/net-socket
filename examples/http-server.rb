424019a727f276f8e5e279ee35f7b7d6101b3843
Ellen Marie Dash <me@duckie.co>
2015-09-28 17:54:43 -0400

    [examples/http-server.rb] Set content length correctly; add comments.

--- examples/http-server.rb
+++ examples/http-server.rb
@@ -12,16 +12,21 @@ socket = TCP::Server.new("0.0.0.0", 8000)
 puts "Server listening #{async ? 'a' : ''}synchronously at 0.0.0.0:8000."
 
 socket.each_request(async) do |conn|
-  # Start headers.
-  conn.write "HTTP/1.0 200 OK\r\n"
-  conn.write "Server: Ruby net::socket example\r\n"
-  conn.write "Content-Type: text/html\r\n"
-  conn.write "Content-Length: 59\r\n"
-  conn.write "\r\n"
-  # End headers
+  # Store response in a variable so that we can get the exact length later.
+  response = "<html><body>Hello from Ruby socket world!</body></html>"
 
-  # Content
-  conn.write "<html><body>Hello, from the Ruby socket world!</body></html>"
+  # Protocol, version, and status code. 
+  conn.write "HTTP/1.0 200 OK\r\n"
+  
+  # Start headers.
+  conn.write "Server: Ruby Met::Socket example\r\n"
+  conn.write "Content-Type: text/html\r\n"
+  conn.write "Content-Length: #{response.length}\r\n"
+  conn.write "\r\n"
+  # End headers.
+
+  # Content.
+  conn.write response
 end
 
 sleep
