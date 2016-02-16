0b41a7431678e50c819ae0800691c5cb1850fe3f
Ellen Marie Dash <me@duckie.co>
2016-02-16 15:36:56 -0500

    Enable TCP::Client.

--- lib/net/socket/tcp.rb
+++ lib/net/socket/tcp.rb
@@ -2,7 +2,7 @@ require 'net/socket/version'
 
 module Net::Socket
   module TCP
-    #require 'net/socket/tcp/client'
+    require 'net/socket/tcp/client'
     require 'net/socket/tcp/server'
   end
 end
