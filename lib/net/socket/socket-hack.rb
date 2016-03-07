0d23182963f7f4116c4c28aa15b04848bf5c3d1f
Ellen Marie Dash <me@duckie.co>
2016-03-07 06:03:44 -0500

    Add documentation about the Socket hack.

--- lib/net/socket/socket-hack.rb
+++ lib/net/socket/socket-hack.rb
@@ -1,6 +1,12 @@
 require 'net/http'
 require 'net/ftp'
 
+# Net::HTTP and Net::FTP reference +Socket+, so wind up referencing
+# +Net::Socket+ normally.
+#
+# This defines +Net::HTTP::Socket+ and +Net::FTP::Socket+ as aliases
+# for +::Socket+, thus avoiding errors.
+
 class Net::HTTP
   Socket = ::Socket
 end
